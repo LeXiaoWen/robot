e3203b36384b75a98e5f55bc93308c73446745c1
LeXiaoWen <LeXiaoWen_Android@163.com>
2019-06-20 01:22:20 +0800

    1.新增机械臂常量配置 2.新增baseFragment

--- app/src/main/java/com/leo/robot/constant/URConstants.java
+++ app/src/main/java/com/leo/robot/constant/URConstants.java
@@ -6,4 +6,76 @@ package com.leo.robot.constant;
 
 
 public class URConstants {
+    public static  String Marm = "Marm";
+    public static  String Farm = "Farm";
+
+    //关节控制
+    public static String ACTION_J0_1 = "ACTION_J0_1";
+    public static String ACTION_J0_2 = "ACTION_J0_2";
+    public static String ACTION_J1_1 = "ACTION_J1_1";
+    public static String ACTION_J1_2 = "ACTION_J1_2";
+    public static String ACTION_J2_1 = "ACTION_J2_1";
+    public static String ACTION_J2_2 = "ACTION_J2_2";
+    public static String ACTION_J3_1 = "ACTION_J3_1";
+    public static String ACTION_J3_2 = "ACTION_J3_2";
+    public static String ACTION_J4_1 = "ACTION_J4_1";
+    public static String ACTION_J4_2 = "ACTION_J4_2";
+    public static String ACTION_J5_1 = "ACTION_J5_1";
+    public static String ACTION_J5_2 = "ACTION_J5_2";
+
+    //控制末端位移
+    public static String ACTION_MOVE_1 ="ACTION_MOVE_1";
+    public static String ACTION_MOVE_2 ="ACTION_MOVE_2";
+    public static String ACTION_MOVE_3 ="ACTION_MOVE_3";
+    public static String ACTION_MOVE_4 ="ACTION_MOVE_4";
+    public static String ACTION_MOVE_5 ="ACTION_MOVE_5";
+    public static String ACTION_MOVE_6 ="ACTION_MOVE_6";
+
+    //控制末端位姿
+    public static String ACTION_POSE_1 = "ACTION_POSE_1";
+    public static String ACTION_POSE_2 = "ACTION_POSE_2";
+    public static String ACTION_POSE_3 = "ACTION_POSE_3";
+    public static String ACTION_POSE_4 = "ACTION_POSE_4";
+    public static String ACTION_POSE_5 = "ACTION_POSE_5";
+    public static String ACTION_POSE_6 = "ACTION_POSE_6";
+
+
+    //DashBoard 命令
+    public static String CMD_POWER_ON = "CMD_POWER_ON";
+    public static String CMD_POWER_OFF = "CMD_POWER_OFF";
+    public static String CMD_UNLOCK_STOP = "CMD_UNLOCK_STOP";
+    public static String CMD_SHUT_DOWN = "CMD_SHUT_DOWN";
+
+    //读取末端位姿参数
+    public static String Act_X = "Act_X";
+    public static String Act_Y = "Act_Y";
+    public static String Act_Z = "Act_Z";
+    public static String Act_Rx = "Act_Rx";
+    public static String Act_Ry = "Act_Ry";
+    public static String Act_Rz = "Act_Rz";
+
+    //读取关节角度
+    public static String J0_A = "J0_A";
+    public static String J1_A = "J1_A";
+    public static String J2_A = "J2_A";
+    public static String J3_A = "J3_A";
+    public static String J4_A = "J4_A";
+    public static String J5_A = "J5_A";
+
+    //读取关节温度
+    public static String J0_T = "J0_T";
+    public static String J1_T = "J1_T";
+    public static String J2_T = "J2_T";
+    public static String J3_T = "J3_T";
+    public static String J4_T = "J4_T";
+    public static String J5_T = "J5_T";
+
+    //状态数据
+    public static String Robot_Mod = "Robot_Mod";
+    public static String Safe_Mod = "Safe_Mod";
+    public static String Program_State = "Program_State";
+    public static String Power_A = "Power_A";
+    public static String Power_V = "Power_V";
+
+
 }
--- app/src/main/java/com/leo/robot/ui/setting/fragment/BaseFragment.java
+++ app/src/main/java/com/leo/robot/ui/setting/fragment/BaseFragment.java
@@ -1,9 +1,59 @@
 package com.leo.robot.ui.setting.fragment;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * created by Leo on 2019/6/20 01 : 00
  */
 
 
-public class BaseFragment {
+public abstract class BaseFragment extends Fragment {
+
+    protected Timer mTimer;
+    protected TimerTask mTimerTask;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mTimer = new Timer();
+        mTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+            }
+        };
+
+        mTimer.schedule(mTimerTask, 1000, 200);//延时1s，每隔500毫秒执行一次run方法
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+               updateData();
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    public abstract void updateData();
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mTimer.cancel();
+        mTimer = null;
+        mTimerTask.cancel();
+        mTimerTask = null;
+    }
+
 }
