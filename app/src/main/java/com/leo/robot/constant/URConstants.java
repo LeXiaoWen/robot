efc221b54dc1cabdfe47391af28233adbfd3bf1d
LeXiaoWen <LeXiaoWen_Android@163.com>
2019-06-20 01:20:56 +0800

    1.新增机械臂常量类 2.新增baseFragment

---
+++ app/src/main/java/com/leo/robot/constant/URConstants.java
@@ -0,0 +1,9 @@
+package com.leo.robot.constant;
+
+/**
+ * created by Leo on 2019/6/19 23 : 20
+ */
+
+
+public class URConstants {
+}
---
+++ app/src/main/java/com/leo/robot/ui/setting/fragment/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.leo.robot.ui.setting.fragment;
+
+/**
+ * created by Leo on 2019/6/20 01 : 00
+ */
+
+
+public class BaseFragment {
+}
