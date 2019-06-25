c4262d1262bc109eb352eea2f6a9beb4e0dbedd5
LeXiaoWen <LeXiaoWen_Android@163.com>
2019-06-25 23:00:25 +0800

    初步解决视频切换问题

--- app/src/main/java/com/leo/robot/utils/ClearWebUtils.java
+++ app/src/main/java/com/leo/robot/utils/ClearWebUtils.java
@@ -1,9 +1,28 @@
 package com.leo.robot.utils;
 
+import android.content.Context;
+import android.webkit.WebView;
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.AgentWebConfig;
+
 /**
  * created by Leo on 2019/6/25 22 : 55
  */
 
 
 public class ClearWebUtils {
+    public static void clearVideo(AgentWeb agentWeb, Context context) {
+        if (agentWeb!=null){
+            WebView webView = agentWeb.getWebCreator().getWebView();
+            webView.resumeTimers();
+            webView.setWebChromeClient(null);
+            webView.setWebViewClient(null);
+            webView.setTag(null);
+            webView.clearHistory();
+            webView.destroy();
+            webView = null;
+            AgentWebConfig.clearDiskCache(context);
+        }
+
+    }
 }
